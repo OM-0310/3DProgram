aa29dfbdc1227c5d3cad398305ff163ba6396612
OM-0310 <kd1335285@st.kobedenshi.ac.jp>
2024-06-14 14:49:19 +0900

    L03_座標変換
    6月14日

--- L03_座標変換/Src/Application/GameObject/Character/Character.cpp
+++ L03_座標変換/Src/Application/GameObject/Character/Character.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Character.h"
-#include "Application/main.h"
+#include "../Terrain/Terrain.h"
+#include "../../main.h"
 
 void Character::Init()
 {
@@ -18,27 +19,53 @@ void Character::Init()
 
 void Character::Update()
 {
+	// ①マウス座標を取得する
 	GetCursorPos(&m_mousePos);
 	ScreenToClient(Application::Instance().GetWindowHandle(), &m_mousePos);
-	m_mousePos.x -= 640;
-	m_mousePos.y -= 360;
-	m_mousePos.y *= -1;
+
+	if (GetAsyncKeyState(VK_RBUTTON) & 0x8000)
+	{
+		std::shared_ptr<KdCamera> _camera = m_wpCamera.lock();
+		if (_camera)
+		{
+			Math::Vector3 _camPos = _camera->GetCameraMatrix().Translation();
+			Math::Vector3 _rayDir = Math::Vector3::Zero;
+			float _rayRange = 1000.0f;
+			// ②レイの発射方向を求める(_rayDir)
+			_camera->GenerateRayInfoFromClientPos(m_mousePos, _camPos, _rayDir, _rayRange);
+
+			// ③実際にレイを飛ばして衝突位置を求める
+			const std::shared_ptr<KdGameObject> _terrain = m_wpTerrain.lock();
+			if (_terrain)
+			{
+				Math::Vector3 _endRayPos = _camPos + (_rayDir * _rayRange);
+				KdCollider::RayInfo _rayInfo(KdCollider::TypeGround, _camPos, _endRayPos);
+
+				// 実際の当たり判定の処理
+				std::list<KdCollider::CollisionResult> _results;
+				_terrain->Intersects(_rayInfo, &_results);
+
+				// 結果が一つでも帰って来ていたら
+				if (_results.size())
+				{
+					for (const KdCollider::CollisionResult& result : _results)
+					{
+						m_TargetPos = result.m_hitPos;
+					}
+				}
+			}
+		}
+	}
 
 	// キャラクターの移動速度(真似しちゃダメですよ)
 	float moveSpd = 0.05f;
-	Math::Vector3 nowPos = m_mWorld.Translation();
+	Math::Vector3 nowPos = GetPos();
+	Math::Vector3 moveVec = m_TargetPos - nowPos;
+	if (moveVec.Length() < moveSpd)moveSpd = moveVec.Length();
 
-	Math::Vector3 moveVec = Math::Vector3::Zero;
-	if (GetAsyncKeyState('D')) { moveVec.x = 1.0f; }
-	if (GetAsyncKeyState('A')) { moveVec.x = -1.0f; }
-	if (GetAsyncKeyState('W')) { moveVec.z = 1.0f; }
-	if (GetAsyncKeyState('S')) { moveVec.z = -1.0f; }
 	moveVec.Normalize();
 	moveVec *= moveSpd;
 	nowPos += moveVec;
-
-	KdCollider::RayInfo ray;
-	m_wpCamera.lock()->GenerateRayInfoFromClientPos(m_mousePos, ray.m_pos, ray.m_dir,ray.m_range);
 
 	// キャラクターのワールド行列を創る処理
 	m_mWorld = Math::Matrix::CreateTranslation(nowPos);
@@ -58,4 +85,6 @@ void Character::DrawSprite()
 	m_wpCamera.lock()->ConvertWorldToScreenDetail(nowPos, charaPos);
 
 	KdShaderManager::Instance().m_spriteShader.DrawTex(m_spTex, charaPos.x, charaPos.y);
+
+	// 2D座標を3D座標へ変換
 }
--- L03_座標変換/Src/Application/GameObject/Character/Character.h
+++ L03_座標変換/Src/Application/GameObject/Character/Character.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+class Terrain;
+
 class Character : public KdGameObject
 {
 public:
@@ -12,11 +14,15 @@ public:
 	void DrawSprite()		override;
 
 	void SetCamera(std::shared_ptr<KdCamera> camera) { m_wpCamera = camera; }
+	void SetTerrain(std::shared_ptr<Terrain> terrain) { m_wpTerrain = terrain; }
 
 private:
 	std::shared_ptr<KdTexture>			m_spTex = nullptr;
 	std::shared_ptr<KdSquarePolygon>	m_spPoly = nullptr;
 	std::weak_ptr<KdCamera>				m_wpCamera;
+	std::weak_ptr<Terrain>				m_wpTerrain;
+
+	Math::Vector3 m_TargetPos = Math::Vector3::Zero;
 
 	POINT m_mousePos;
 };
