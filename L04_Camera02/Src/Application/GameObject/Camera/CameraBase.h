b0660e3c5ce051bb49fb40aae9e47568d662b659
OM-0310 <kd1335285@st.kobedenshi.ac.jp>
2024-06-19 11:27:12 +0900

    L04_Camera02
    6月19日　FPSCamera追加

--- L04_Camera02/Src/Application/GameObject/Camera/CameraBase.h
+++ L04_Camera02/Src/Application/GameObject/Camera/CameraBase.h
@@ -11,5 +11,38 @@ public:
 	void Update()			override;
 	void PreDraw()			override;
 
+	void SetTarget(const std::shared_ptr<KdGameObject>& target)
+	{
+		m_wpTarget = target;
+	}
+
+	const Math::Matrix GetRotationMatrix() const
+	{
+	//									y	x	z　の順
+		return Math::Matrix::CreateFromYawPitchRoll(
+			DirectX::XMConvertToRadians(m_DegAng.y),
+			DirectX::XMConvertToRadians(m_DegAng.x),
+			DirectX::XMConvertToRadians(m_DegAng.z));
+	}
+
+	const Math::Matrix GetRotationYMatrix() const
+	{
+		return Math::Matrix::CreateRotationY(DirectX::XMConvertToRadians(m_DegAng.y));
+	}
+
 private:
+	// カメラの回転速度
+	Math::Vector3				m_DegAng	= Math::Vector3::Zero;
+
+protected:
+	void UpdateRotateByMouse();
+
+	std::shared_ptr<KdCamera>	m_spCamera	= nullptr;
+	std::weak_ptr<KdGameObject> m_wpTarget;
+
+	Math::Matrix				m_mLocalPos = Math::Matrix::Identity;
+	Math::Matrix				m_mRotation = Math::Matrix::Identity;
+
+	// カメラ回転用マウス座標
+	POINT						m_FixMousePos{};
 };
--- L04_Camera02/Src/Application/main.cpp
+++ L04_Camera02/Src/Application/main.cpp
@@ -2,6 +2,9 @@
 
 #include "GameObject/Terrain/Terrain.h"
 #include "GameObject/Character/Character.h"
+
+#include "GameObject/Camera/TrackingCamera/TrackingCamera.h"
+#include "GameObject/Camera/FPSCamera/FPSCamera.h"
 // ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
 // エントリーポイント
 // アプリケーションはこの関数から進行する
@@ -198,9 +201,9 @@ bool Application::Init(int w, int h)
 	// フルスクリーン確認
 	//===================================================================
 	bool bFullScreen = false;
-	if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
-		bFullScreen = true;
-	}
+	//if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
+	//	bFullScreen = true;
+	//}
 
 	//===================================================================
 	// Direct3D初期化
@@ -242,10 +245,6 @@ bool Application::Init(int w, int h)
 	KdAudioManager::Instance().Init();
 
 	//===================================================================
-	// カメラ初期化
-	//===================================================================
-
-	//===================================================================
 	// ステージ初期化
 	//===================================================================
 	std::shared_ptr<Terrain> _terrain = std::make_shared<Terrain>();
@@ -258,6 +257,15 @@ bool Application::Init(int w, int h)
 	std::shared_ptr<Character> _character = std::make_shared<Character>();
 	_character->Init();
 	m_GameObjectList.push_back(_character);
+
+	//===================================================================
+	// カメラ初期化
+	//===================================================================
+	std::shared_ptr<FPSCamera> _camera = std::make_shared<FPSCamera>();
+	_camera->Init();
+	_camera->SetTarget(_character);
+	_character->SetCamera(_camera);
+	m_GameObjectList.push_back(_camera);
 
 	return true;
 }
