b71685d9f3b37d677ef74211751ed7160048c48b
OM-0310 <kd1335285@st.kobedenshi.ac.jp>
2024-11-14 09:11:40 +0900

    L11_WaterShader自分用

---
+++ L11_WaterShader自分用/Src/Application/GameObject/Water/Water.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class Water : public KdGameObject
+{
+public:
+
+	Water			()				{}
+	~Water			()	override	{}
+
+	void Init		()	override;
+	void Update		()	override;
+	void DrawLesson	()	override;
+
+private:
+
+	std::shared_ptr<KdModelWork>	m_spModel;
+
+	Math::Matrix					m_mTrans;
+
+	Math::Vector3					m_pos;
+
+	Math::Vector2					m_UVOffset;
+	Math::Matrix					m_mR;
+};
